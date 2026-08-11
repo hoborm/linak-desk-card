29f5f6c7ed6e9274b6b4b71df7238e1c9414aa6b
Mihály Hóbor <hoborm0@gmail.com>
2026-08-12 00:59:26 +0200

    correct rollup.config.js to output to root

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,11 @@
 import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
-import minifyHTML from 'rollup-plugin-minify-html-literals';
 import { terser } from 'rollup-plugin-terser';
 import serve from 'rollup-plugin-serve';
 import alias from '@rollup/plugin-alias';
 import json from '@rollup/plugin-json';
 import image from '@rollup/plugin-image';
-import del from 'rollup-plugin-delete'
+import del from 'rollup-plugin-delete';
 
 const dev = process.env.ROLLUP_WATCH;
 
@@ -14,32 +13,34 @@ export default [
   {
     input: 'src/linak-desk-card.ts',
     output: {
-      dir: 'dist',
+      file: 'linak-desk-card.js',
       format: 'es',
     },
     plugins: [
-      del({ targets: 'dist/*' }),
-      minifyHTML(),
+      del({ targets: 'linak-desk-card.js' }),
       nodeResolve({}),
       typescript(),
       json(),
       image(),
       alias({
-        entries: [{
-          find: 'lit-html/lib/shady-render.js',
-          replacement: 'node_modules/lit-html/lit-html.js'
-        }]
+        entries: [
+          {
+            find: 'lit-html/lib/shady-render.js',
+            replacement: 'node_modules/lit-html/lit-html.js',
+          },
+        ],
       }),
       terser({ output: { comments: false } }),
-      dev && serve({
-        contentBase: ['./dist'],
-        host: '0.0.0.0',
-        port: 5000,
-        allowCrossOrigin: true,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-      }),
+      dev &&
+        serve({
+          contentBase: ['./'],
+          host: '0.0.0.0',
+          port: 5000,
+          allowCrossOrigin: true,
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+          },
+        }),
     ],
   },
 ];
